e895e304bb37413a35618cddb0b7d139d45170dc
Oshith Roshantha <oedirisuriya@gmail.com>
2024-06-08 22:05:15 +0530

    addTast added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 import Dashboard from './Pages/Dashboard';
 import Login from './Pages/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddTask from './Components/AddTask';
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+    {/*  <BrowserRouter>
         <Routes>
           <Route path='/*' element={<Login/>}></Route>
           <Route path='Dashboard' element={<Dashboard/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <AddTask/>
     </div>
   );
 }
