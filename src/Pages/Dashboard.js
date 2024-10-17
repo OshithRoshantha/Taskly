7923ae86992b40affe2c06876ec0b3c512ecd84e
Oshith Roshantha <oedirisuriya@gmail.com>
2024-10-17 15:06:55 +0530

    Refactor CardPreview component, update styles, and add status dropdown

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -12,7 +12,16 @@ export default function Dashboard() {
         <div className='head-strip h4 mb-0'>Dashboard</div>
         <div className='dashboard-container'>
             <div className='dashboard-item1'>
-                <div className='head-text'><p className='b'>To do</p><i class="bi bi-plus-lg addBtn"></i></div>
+                <div className='head-text'>
+                    <p className='b'>To do</p>
+                    <div className='sort-by-full'>
+                        <i class="bi bi-sort-alpha-down"></i>&nbsp;&nbsp;
+                        <select className='sort-by'>
+                            <option selected>Due Date</option>
+                            <option>Priority</option>
+                        </select> 
+                    </div>
+                </div>
                 <div className='item-inner'>
                     <CardPreview title={title} description={description} priority={priority} date={date} status={"To do"}/> 
                     <CardPreview title={title} description={description} priority={priority} date={date} status={"To do"}/> 
@@ -21,14 +30,32 @@ export default function Dashboard() {
                 <p className='add-task' align='center'><i class="bi bi-plus-lg"></i>&nbsp;Add Task</p>
             </div>
             <div className='dashboard-item2'>
-                <div className='head-text'><p className='b'>In progress</p><i class="bi bi-plus-lg addBtn"></i></div>
+                <div className='head-text head-text2'>
+                    <p className='b'>In progress</p>
+                    <div className='sort-by-full'>
+                        <i class="bi bi-sort-alpha-down"></i>&nbsp;&nbsp;
+                        <select className='sort-by'>
+                            <option selected>Due Date</option>
+                            <option>Priority</option>
+                        </select> 
+                    </div>
+                </div>
                 <div className='item-inner'>
                     <CardPreview title={title} description={description} priority={priority} date={date} status={"In progress"}/> 
                 </div>  
                 <p className='add-task' align='center'><i class="bi bi-plus-lg"></i>&nbsp;Add Task</p>       
             </div>
             <div className='dashboard-item3'>
-                <div className='head-text'><p className='b'>Done</p><i class="bi bi-plus-lg addBtn"></i></div>
+                <div className='head-text'>
+                    <p className='b'>To do</p>
+                    <div className='sort-by-full'>
+                        <i class="bi bi-sort-alpha-down"></i>&nbsp;&nbsp;
+                        <select className='sort-by'>
+                            <option selected>Done</option>
+                            <option>Priority</option>
+                        </select> 
+                    </div>
+                </div>
                 <div className='item-inner'>
                     <CardPreview title={title} description={description} priority={priority} date={date} status={"Done"}/> 
                     <CardPreview title={title} description={description} priority={priority} date={date} status={"Done"}/> 
