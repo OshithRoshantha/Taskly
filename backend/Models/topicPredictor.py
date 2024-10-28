36db73df39851943ba5ee9fd53da8d46da8f69c1
Oshith Roshantha <oedirisuriya@gmail.com>
2024-10-28 21:49:33 +0530

    Refactor topicPredictor.py to use a different summarization model for text summarization

--- backend/Models/topicPredictor.py
+++ backend/Models/topicPredictor.py
@@ -4,7 +4,7 @@ class predictor:
         self.summary=summary
         
     def model(self):
-        summarizer=pipeline("summarization", model="facebook/bart-large-cnn")
-        topic=summarizer(self.summary,max_length=12,min_length=12,do_sample=False)
+        summarizer=pipeline("summarization", model="t5-small")
+        topic=summarizer(self.summary, max_length=15, min_length=7, do_sample=False)
         
         return topic[0]['summary_text']
